8fb1df60ed3c32e82d9b74f4fb4ad38c7e1bed6b
Maxi Streifeneder <maximilian.streifeneder@sap.com>
2020-09-21 16:39:50 +0200

    added event emitting

--- cap/brain/srv/service.js
+++ cap/brain/srv/service.js
@@ -1,9 +1,19 @@
-module.exports = async srv => {
+module.exports = async (srv) => {
+  const messaging = await cds.connect.to("messaging");
 
-  const messaging = await cds.connect.to('messaging')
+  messaging.on("salesorder/created", (msg) => {
+    console.log("SALESORDER", JSON.stringify(msg));
+  });
 
-  messaging.on('salesorder/created', msg => {
-    console.log('SALESORDER', JSON.stringify(msg))
-  })
+  srv.on("invoke", async (req) => {
+    let tx = messaging.tx(req);
+    tx.emit("CAP/FunctionInvoked", { customerID: "TestCustomer" });
 
-}
+    await messaging.tx(req).emit({
+      event: "CAP/Function/Invoked",
+      data: { test: "banana" },
+    });
+
+    return "OK";
+  });
+};
