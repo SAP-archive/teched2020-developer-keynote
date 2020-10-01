1c4b012123218e36107d7bfda36057a08abe022b
DJ Adams <dj.adams@sap.com>
2020-10-01 08:21:57 +0100

    tweak mock converter

--- converter/mock/index.js
+++ converter/mock/index.js
@@ -1,14 +1,20 @@
-// Mock converter service
-// Test with e.g. http://localhost:5000/conversion/123.45
+// Mock converter service; returns given (stringified) number
+// with 42 added to it. Returned with two decimal places.
+
+// Example:
+// > GET http://localhost:5000/conversion/100.95
+// < {"value":"142.95"}
 
 const PORT = process.env.PORT || 5000
 
 require('express')()
   .get('/conversion/:amount', (req, res) => {
     console.log('☞', req.path)
+    const converted = Number(req.params.amount) + 42
+    const formatted = (Math.round(converted * 100) / 100).toFixed(2)
     res.setHeader('Content-Type', 'application/json')
     res.end(JSON.stringify({
-      value: Number(req.params.amount) + 42
+      value: formatted
     }))
   })
   .listen(
