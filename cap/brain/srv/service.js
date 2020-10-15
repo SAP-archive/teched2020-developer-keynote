669c42b50a00e72eec4ad20f537ce117fcc9cf23
Maxi Streifeneder <maximilian.streifeneder@sap.com>
2020-10-15 16:34:13 +0200

    keynote date for abap event

--- cap/brain/srv/service.js
+++ cap/brain/srv/service.js
@@ -80,10 +80,13 @@ module.exports = async (srv) => {
     // --------------------------------------------------------------
 
     // Convert creation date from OData v2 wrapped epoch to yyyy-mm-dd
-    const creationYyyyMmDd =
+    /*const creationYyyyMmDd =
       new Date(Number(result.CreationDate.replace(/[^\d]/g, '')))
         .toISOString()
-        .slice(0,10)
+        .slice(0,10)*/
+    
+    //keynote Date simulation
+    const creationYyyyMmDd = new Date("2020-12-08").toISOString.slice(0,10);
 
     // Create event payload
     const eventData = charityfund.increased({
