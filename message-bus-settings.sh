01f294ac582be5920fdf44812d6370cb35573dcd
DJ Adams <dj.adams@sap.com>
2020-12-31 09:43:38 +0000

    elevate settings to shared place

---
+++ message-bus-settings.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# SAP Enterprise Messaging service settings common all the relevant
+# components in the repository - settings.sh
+
+# shellcheck disable=SC2034
+service=enterprise-messaging
+plan=dev
+instance=emdev
+servicekey=sk1
