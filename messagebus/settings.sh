cd4680e4717b361a7fec6d45dad14e160d764b54
DJ Adams <dj.adams@sap.com>
2020-12-31 09:47:55 +0000

    point settings.sh to shared

--- messagebus/settings.sh
+++ messagebus/settings.sh
@@ -1,9 +1,1 @@
-#!/usr/bin/env bash
-
-# SETTINGS - source this file in each script
-
-# shellcheck disable=SC2034
-service=enterprise-messaging
-plan=dev
-instance=emdev
-servicekey=sk1
+../message-bus-settings.sh
